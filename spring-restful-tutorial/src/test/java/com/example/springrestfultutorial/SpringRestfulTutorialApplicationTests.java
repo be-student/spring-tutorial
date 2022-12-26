1a765fde61185f41e29a14af56679081eafdb209
Be-student <presentsong@naver.com>
2022-12-26 16:13:59 +0900

    2.7.7 ga 버전으로 변경
    3.0.1 버전 실행이 안 됨

--- spring-restful-tutorial/src/test/java/com/example/springrestfultutorial/SpringRestfulTutorialApplicationTests.java
+++ spring-restful-tutorial/src/test/java/com/example/springrestfultutorial/SpringRestfulTutorialApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.springrestfultutorial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringRestfulTutorialApplicationTests {
 
 	@Test
 	void contextLoads() {
